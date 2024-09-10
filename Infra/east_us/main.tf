6895c0195dfb7e043ee9920619920bc7097aeee3
terese.mathew <terese.mathew@outlook.com>
2024-09-10 18:39:36 +0530

    updated

--- Infra/east_us/main.tf
+++ Infra/east_us/main.tf
@@ -14,7 +14,7 @@ retention_in_days = var.retention_in_days
 
 
 /*module "app_insights"{
-source= "../modules/log_analytics"
+source= "../modules/app_insights"
 location=var.location
 application_insights_name = var.application_insights_name
 resource_group_name = var.resource_group_name
