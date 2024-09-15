9a3f7eee428282b9862636e3b673fa9608524223
terese.mathew <terese.mathew@outlook.com>
2024-09-15 22:34:00 +0530

    updated folders

--- Infra/east_us/main.tf
+++ Infra/east_us/main.tf
@@ -4,7 +4,7 @@
 /***********Module for Log Analytics Workspace Creation********************************/
 
 module "log_analytics" {
-source= "../../modules/log_analytics"
+source= "./modules/log_analytics"
 location=var.location
 log_analytics_name = var.log_analytics_name
 resource_group_name = data.azurerm_resource_group.rg.name
@@ -15,7 +15,7 @@ retention_in_days = var.retention_in_days
 /***********Module for Application Insights Creation********************************/
 
 module "app_insights"{
-source= "../../modules/app_insights"
+source= "./modules/app_insights"
 location=var.location
 application_insights_name = var.application_insights_name
 resource_group_name = data.azurerm_resource_group.rg.name
@@ -23,7 +23,7 @@ application_type =var.application_type
 }
 
 module "apim_instance"{
-source= "../../modules/apim_instance"
+source= "./modules/apim_instance"
 location=var.location
 resource_group_name = data.azurerm_resource_group.rg.name
 apim_instance_name  = var.apim_instance_name
