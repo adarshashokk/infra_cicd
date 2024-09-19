cb68a0a9c5dd0af5f7a4336ffa633686688e5360
adarshashok6@hotmail.com <adarshashok6@hotmail.com>
2024-09-19 15:34:08 +0530

    modified:   Infra/east_us/generate_backend.sh

--- Infra/east_us/generate_backend.sh
+++ Infra/east_us/generate_backend.sh
@@ -1,15 +1,15 @@
 
-#!/bin/bash
+# #!/bin/bash
 
-# Create the backend.tf file
-cat <<EOF > backend.tf
-terraform {
-  backend "azurerm" {
-    resource_group_name      = "$BACKEND_RESOURCE_GROUP_NAME"
-    storage_account_name     = "$BACKEND_STORAGE_ACCOUNT_NAME"
-    container_name           = "$BACKEND_CONTAINER_NAME"
-    key                      = "$BACKEND_KEY/terraform.tfstate"
-    access_key               = "$AZURE_STORAGE_ACCESS_KEY"
-  }
-}
-EOF
+# # Create the backend.tf file
+# cat <<EOF > backend.tf
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name      = "$BACKEND_RESOURCE_GROUP_NAME"
+#     storage_account_name     = "$BACKEND_STORAGE_ACCOUNT_NAME"
+#     container_name           = "$BACKEND_CONTAINER_NAME"
+#     key                      = "$BACKEND_KEY/terraform.tfstate"
+#     access_key               = "$AZURE_STORAGE_ACCESS_KEY"
+#   }
+# }
+# EOF
--- Infra/east_us/provider.tf
+++ Infra/east_us/provider.tf
@@ -4,7 +4,7 @@ terraform {
     organization = "Adarshk" 
 
     workspaces { 
-      name = "develops02" 
+      name = "develop01" 
     } 
   }
 
